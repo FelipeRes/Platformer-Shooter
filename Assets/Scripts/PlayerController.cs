494cd7a94d3b7e22362d84094cbaf1ff105b5d31
Felipe Resende Gomes <feliperesendehq@outlook.com>
2025-10-17 00:37:21 -0300

    Tutorial 4 - Dynamic character controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem; // Add the Input System
 public class PlayerController : MonoBehaviour
 {
 	public float speed;
+	public float jumpForce;
+	public Rigidbody2D rigidbody;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,21 +16,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.wKey.isPressed) {
-			this.transform.position += Vector3.up * Time.deltaTime * speed;
-		}
-
-		if (Keyboard.current.sKey.isPressed) {
-			this.transform.position += Vector3.down * Time.deltaTime * speed;
-		}
 
 		if (Keyboard.current.dKey.isPressed) {
-			this.transform.position += Vector3.right * Time.deltaTime * speed;
+			rigidbody.linearVelocityX = speed;
+		} else if (Keyboard.current.aKey.isPressed) {
+			rigidbody.linearVelocityX = -speed;
+		} else {
+			rigidbody.linearVelocityX = 0;
 		}
 
-		if (Keyboard.current.aKey.isPressed) {
-			this.transform.position += Vector3.left * Time.deltaTime * speed;
+		if (Keyboard.current.spaceKey.wasPressedThisFrame) {
+			rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 		}
+
+
 
 	}
 }
